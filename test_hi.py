399f4ed09d2ad98e1c7182a438d23b9a2eb1a1a7
bharathsp0305 <bharath.dhanabalan@phantom-fx.com>
2024-08-09 16:03:03 +0530

    Update test_hi.py

--- test_hi.py
+++ test_hi.py
@@ -1,8 +1,10 @@
 from hi import add
+import pytest
 
 
 def test_answer():
     assert add(3,2) == 5
 
 def test_false_answer():
-    assert add(3,5) != 8
+    with pytest.raises(AssertionError):
+        assert add(3,5) != 8
